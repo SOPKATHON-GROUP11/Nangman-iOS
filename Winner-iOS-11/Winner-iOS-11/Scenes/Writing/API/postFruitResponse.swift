882c48416ae9beb76b2b22ad739da88435f997e3
Sejin Lee <lsj8706@naver.com>
2022-05-22 05:30:15 +0900

    [Feat] postFruit API 연결

---
+++ Winner-iOS-11/Winner-iOS-11/Scenes/Writing/API/postFruitResponse.swift
@@ -0,0 +1,14 @@
+//
+//  postFruitResponse.swift
+//  Winner-iOS-11
+//
+//  Created by User on 2022/05/22.
+//
+
+import Foundation
+
+struct postFruitResponse: Codable {
+    var status: Int
+    var success: Bool
+    var message: String
+}
