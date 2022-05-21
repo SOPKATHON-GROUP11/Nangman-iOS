ed2d30daadd3e0c118a337520ae71d1d1a46d65a
dev-madilyn <lollin0@naver.com>
2022-05-22 02:18:14 +0900

    [Feat] Floating Button

---
+++ Winner-iOS-11/Winner-iOS-11/Scenes/MyTree/FloatingButtonVC.swift
@@ -0,0 +1,49 @@
+//
+//  FloatingButtonVC.swift
+//  Winner-iOS-11
+//
+//  Created by madilyn on 2022/05/22.
+//
+
+import UIKit
+
+class FloatingButtonVC: UIViewController {
+    
+    
+    @IBOutlet weak var appleButton: UIButton!
+    @IBOutlet weak var persimmonButton: UIButton!
+    
+    @IBOutlet weak var appleCenterY: NSLayoutConstraint!
+    @IBOutlet weak var persimmonCenterY: NSLayoutConstraint!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUI()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.1, initialSpringVelocity: 0, options: .curveEaseOut) {
+            
+            self.appleCenterY.constant = 80
+            self.persimmonCenterY.constant = 160
+            
+            self.view.layoutIfNeeded()  //이걸 꼭 써줘야 갱신이 됨.
+        } completion: { completion in
+            //애니메이션이 끝나는 시점에서 실행
+        }
+    }
+
+    @IBAction func didTapDismissButton(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    @IBAction func didTapBackgroundView(_ sender: Any) {
+        self.dismiss(animated: true)
+    }
+    
+    private func setUI() {
+        appleCenterY.constant = 0
+        persimmonCenterY.constant = 0
+    }
+}
