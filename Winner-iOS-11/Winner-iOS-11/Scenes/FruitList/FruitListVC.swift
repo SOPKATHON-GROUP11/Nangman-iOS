191613ead3857b2bdc1b142e3445526fe246e5d0
Choi YoungLyn <joeuni0129@gmail.com>
2022-05-22 03:05:11 +0900

    [Chore] FruitListTableView UI

--- Winner-iOS-11/Winner-iOS-11/Scenes/FruitList/FruitListVC.swift
+++ Winner-iOS-11/Winner-iOS-11/Scenes/FruitList/FruitListVC.swift
@@ -9,21 +9,41 @@ import UIKit
 
 class FruitListVC: UIViewController {
 
+   // MARK: - @IBOutlet Properties
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var fruitListTV: UITableView!
+    
+    var isAll = true
+    
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setTableView() {
+        let fruitListNib = UINib(nibName: FruitListTVC.className, bundle: nil)
+        fruitListTV.register(fruitListNib, forCellReuseIdentifier: FruitListTVC.className)
+        
+        fruitListTV.delegate = self
+        fruitListTV.dataSource = self
     }
-    */
-
 }
+
+extension FruitListVC: UITableViewDelegate{
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+}
+
+extension FruitListVC: UITableViewDataSource {
+   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       return 1
+        //return FruitListDataModel.sampleData.count
+   }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: FruitListTVC.className, for: indexPath)
+        return cell
+    }
+}
+
