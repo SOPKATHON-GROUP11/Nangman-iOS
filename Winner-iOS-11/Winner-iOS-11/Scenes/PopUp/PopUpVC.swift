4ab4d001b199bcbbee57fb484bcc9bce78ea1fff
dev-madilyn <lollin0@naver.com>
2022-05-22 01:32:03 +0900

    [Feat] PopUpVC

--- Winner-iOS-11/Winner-iOS-11/Scenes/PopUp/PopUpVC.swift
+++ Winner-iOS-11/Winner-iOS-11/Scenes/PopUp/PopUpVC.swift
@@ -5,4 +5,39 @@
 //  Created by madilyn on 2022/05/21.
 //
 
-import Foundation
+import UIKit
+
+class PopUpVC: BaseVC {
+
+    // MARK: - @IBOutlet Properties
+    @IBOutlet weak var contentLabel: UILabel!
+    @IBOutlet weak var leftButton: UIButton!
+    @IBOutlet weak var rightButton: UIButton!
+    @IBOutlet weak var containerView: UIView!
+    
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUI()
+    }
+    
+    private func setUI() {
+        self.modalPresentationStyle = .overFullScreen
+        self.modalTransitionStyle = .crossDissolve
+        
+        contentLabel.textAlignment = .center
+    }
+    @IBAction func didTapLeftButton(_ sender: Any) {
+        self.dismiss(animated: true)
+    }
+    
+    func showPopUp(vc: UIViewController, message: String, rightButtonTitle: String, leftButtonTitle: String) {
+        contentLabel.text = message
+        DispatchQueue.main.async {
+            self.rightButton.setTitle(rightButtonTitle, for: .normal)
+            self.leftButton.setTitle(leftButtonTitle, for: .normal)
+        }
+        vc.present(self, animated: true)
+    }
+}
